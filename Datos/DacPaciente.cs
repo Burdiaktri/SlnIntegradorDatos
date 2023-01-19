56ace1a8fb72edbe9f1f11f11ab5e220fa017c85
burdiaktri <burdiaktrinidad33@gmail.com>
2023-01-19 14:10:52 -0300

    'message'

---
+++ Datos/DacPaciente.cs
@@ -0,0 +1,48 @@
+﻿using Datos.Data;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class DacPaciente
+    {
+        private static DBDatosContext context = new DBDatosContext();
+        public static List<Paciente> Select()
+        {
+            return context.Pacientes.ToList();
+        }
+
+        public static Paciente SelectById(int id)
+        {
+            return context.Pacientes.Find(id);
+        }
+
+        public static int Insertar(Paciente paciente)
+        {
+            context.Pacientes.Add(paciente);
+            return context.SaveChanges();
+        }
+        public static int Eliminar(Paciente paciente)
+        {
+            Paciente pacienteOrigen = context.Pacientes.Find(paciente.Id);
+            if (pacienteOrigen != null)
+            {
+                context.Pacientes.Remove(pacienteOrigen);
+                return context.SaveChanges();
+            }
+            return 0;
+        }
+        public static int Update(Paciente paciente)
+        {
+            Paciente pacienteOrigen = context.Pacientes.Find(paciente.Id);
+            pacienteOrigen.Nombre = paciente.Nombre;
+
+            return context.SaveChanges();
+        }
+
+    }
+}
---
+++ Datos/Data/DBDatosContext.cs
@@ -0,0 +1,22 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Data
+{
+    public class DBDatosContext : DbContext
+    {
+        public DBDatosContext() : base("KeyDB")
+        {
+
+        }
+        //Propiedades DBSET
+
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+    }
+}
